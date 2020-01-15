40e449d0fef3b51991c906f8c994be0eb4326de6
mucteb <mucteb@gmail.com>
2020-01-15 11:11:31 -0500

    first commit

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -11,6 +11,8 @@ package ca.sheridancollege.week1.softwarefundamentals;
  */
 public class StudentList {
     
+    public String Derya;
+    
     public static void main(String[] args) {
         
         Student [] sList= new Student[5];
